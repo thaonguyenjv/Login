9f4e53e19d90de94c254d2c5d4d8dfe8113bbc9c
thaonguyenjv~ <nntnguyen1885@gmail.com>
2025-05-05 07:42:30 +0700

    fix leftmenu

--- admin_left_menu.php
+++ admin_left_menu.php
@@ -2,16 +2,16 @@
 if(isset($_SESSION['username']) && isset($_SESSION['quyen'])){
     switch($_SESSION['quyen']){
         case "admin":
-            echo "<a href='admin.php?module=admin'><div class='menu_item'>Module 1: chỉ dành cho A</div></a>";
-            echo "<a href='admin.php?module=mod'><div class='menu_item'>Module 2: chỉ dành cho A, M</div></a>";
-            echo "<a href='admin.php?module=user'><div class='menu_item'>Module 3: chỉ dành cho A, M, U</div></a>";
+            echo "<a href='homepage.php?module=admin'><div class='menu_item'>Module 1: chỉ dành cho A</div></a>";
+            echo "<a href='homgpage.php?module=mod'><div class='menu_item'>Module 2: chỉ dành cho A, M</div></a>";
+            echo "<a href='homepage.php?module=user'><div class='menu_item'>Module 3: chỉ dành cho A, M, U</div></a>";
             break;
         case "mod":
-            echo "<a href='admin.php?module=mod'><div class='menu_item'>Module 2: chỉ dành cho A, M</div></a>";
-            echo "<a href='admin.php?module=user'><div class='menu_item'>Module 3: chỉ dành cho A, M, U</div></a>";
+            echo "<a href='homepage.php?module=mod'><div class='menu_item'>Module 2: chỉ dành cho A, M</div></a>";
+            echo "<a href='homepage.php?module=user'><div class='menu_item'>Module 3: chỉ dành cho A, M, U</div></a>";
             break;
         case "user":
-            echo "<a href='admin.php?module=user'><div class='menu_item'>Module 3: chỉ dành cho A, M, U</div></a>";
+            echo "<a href='homepage.php?module=user'><div class='menu_item'>Module 3: chỉ dành cho A, M, U</div></a>";
             break;
         default:
             break;
